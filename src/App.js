f5cb5eb86adea87b4b1b8b870057dc2a5a5078f0
Collin McCloskey <collin@transportatx.com>
2024-04-29 15:13:02 -0600

    Initial React setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Aria Client!</h1>
+      <p>This is a simple React client setup.</p>
+    </div>
+  );
+}
+
+export default App;
